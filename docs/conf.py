1275c433042fc0fda1569b94de64c785101be030
Siddharth Nathella <56081388+siddn@users.noreply.github.com>
2025-03-18 12:40:04 -0400

    theme correction

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ myst_enable_extensions = [
 html_title = "wfANOVApy"
 
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 htmlhelp_basename = 'SphinxwithMarkdowndoc'
 
